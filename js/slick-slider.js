d94ec66282737d58ab89d60cdb9c24fd58bb9b9e
Winnbor <59263951+Winnbor@users.noreply.github.com>
2021-02-27 02:24:08 +0300

    Add slick slider experiment

---
+++ js/slick-slider.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+  $('.review-slider').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: true,
+    prevArrow: '<button type="button" class="slick-prev"><svg class="icon-arrow-left"><use xlink:href="sprite.svg#arrow-left"></use></svg></button>',
+    nextArrow: '<button type="button" class="slick-next"><svg class="icon-arrow-right"><use xlink:href="sprite.svg#arrow-right"></use></svg></button>',
+    fade: true,
+      asNavFor: '.review-second-slider',
+    infinite: true
+  });
+});
+$('.review-second-slider').slick({
+  slidesToShow: 7,
+  slidesToScroll: 8,
+  arrows: false,
+  asNavFor: '.review-slider',
+  centerMode: false,
+  focusOnSelect: true,
+  infinite: false
+});
