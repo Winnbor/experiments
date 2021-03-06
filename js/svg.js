3bea25d292710ee19dcece17edf16c838ee36efd
Winnbor <59263951+Winnbor@users.noreply.github.com>
2021-03-06 21:39:40 +0300

    Add svg animation experiment

---
+++ js/svg.js
@@ -0,0 +1,16 @@
+new Vivus('my-svg', {
+  type: 'sync',
+  duration: 125,
+  animTimingFunction: Vivus.EASE,
+});
+
+new Vivus('my-svg2', {
+  type: 'sync',
+  duration: 125,
+  animTimingFunction: Vivus.EASE,
+});
+new Vivus('my-svg3', {
+  type: 'sync',
+  duration: 125,
+  animTimingFunction: Vivus.EASE,
+});
